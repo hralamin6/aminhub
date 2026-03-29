26336db228c6519be45897d8b351ccdadaf5ee0b
hr alamin <hralamin2020@gmail.com>
2026-03-29 23:50:32 +0600

    aminhub

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,13 +1,7 @@
 <?php
 
-use App\Providers\AppServiceProvider;
-use App\Providers\BroadcastServiceProvider;
-use App\Providers\HorizonServiceProvider;
-use App\Providers\TelescopeServiceProvider;
-
 return [
-    AppServiceProvider::class,
-    BroadcastServiceProvider::class,
-    HorizonServiceProvider::class,
-    TelescopeServiceProvider::class,
+    App\Providers\AppServiceProvider::class,
+    App\Providers\BroadcastServiceProvider::class,
+    App\Providers\HorizonServiceProvider::class,
 ];
